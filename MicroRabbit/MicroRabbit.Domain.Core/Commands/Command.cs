30fdb5609598004017ec0e43c09f908723c40c9e
Yurij <horyuriy@gmail.com>
2024-02-20 19:44:54 +0200

    Implemented Interfaces and Classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; set; }
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
